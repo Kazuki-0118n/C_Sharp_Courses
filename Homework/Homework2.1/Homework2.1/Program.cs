3efb1b03b35fa6e9b770458d6e4d495927e28c54
Kazuki-0118n <krivogubanastasiya@gmail.com>
2025-12-03 19:41:36 +0200

    Добавила проєкт Homework2.1

---
+++ Homework/Homework2.1/Homework2.1/Program.cs
@@ -0,0 +1,83 @@
+﻿// Order 1
+
+Console.Write("Enter client name: ");
+
+string nameOne = Console.ReadLine();
+
+Console.Write("Enter product: ");
+
+string productOne = Console.ReadLine();
+
+Console.Write("Enter price (only numbers): ");
+
+decimal priceOne = decimal.Parse(Console.ReadLine());
+
+Console.Write("Enter address: ");
+
+string addressOne = Console.ReadLine();
+
+Console.WriteLine("Ordered No 1");
+
+Console.WriteLine($"Client: {nameOne}.");
+
+Console.WriteLine($"Product: {productOne}, price {priceOne} EUR.");
+
+Console.WriteLine($"Address: {addressOne}.");
+
+Console.WriteLine();
+
+// Order 2
+
+Console.Write("Enter client name: ");
+
+string nameTwo = Console.ReadLine();
+
+Console.Write("Enter product: ");
+
+string productTwo = Console.ReadLine();
+
+Console.Write("Enter price (only numbers): ");
+
+decimal priceTwo = decimal.Parse(Console.ReadLine());
+
+Console.Write("Enter address: ");
+
+string addressTwo = Console.ReadLine();
+
+Console.WriteLine("Ordered No 2");
+
+Console.WriteLine($"Client: {nameTwo}.");
+
+Console.WriteLine($"Product: {productTwo}, price {priceTwo} EUR.");
+
+Console.WriteLine($"Address: {addressTwo}.");
+
+Console.WriteLine();
+
+// Order 3
+
+Console.Write("Enter client name: ");
+
+string nameThree = Console.ReadLine();
+
+Console.Write("Enter product: ");
+
+string productThree = Console.ReadLine();
+
+Console.Write("Enter price (only numbers): ");
+
+decimal priceThree = decimal.Parse(Console.ReadLine());
+
+Console.Write("Enter address: ");
+
+string addressThree = Console.ReadLine();
+
+Console.WriteLine("Ordered No 3");
+
+Console.WriteLine($"Client: {nameThree}.");
+
+Console.WriteLine($"Product: {productThree}, price {priceThree} EUR.");
+
+Console.WriteLine($"Address: {addressThree}.");
+
+Console.WriteLine();
