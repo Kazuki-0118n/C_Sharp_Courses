acfd472e91116d590d765a1085ed706c62ce7071
Kazuki-0118n <krivogubanastasiya@gmail.com>
2025-12-10 15:14:57 +0200

    Add Homework5.1_3

--- Homework/Homework5.1/Homework5.1_3/Homework5.1_3/Homework5.1_3/Program.cs
+++ Homework/Homework5.1/Homework5.1_3/Homework5.1_3/Homework5.1_3/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Enter a number: ");
+
+string strUserNumber = Console.ReadLine();
+int userNumber = 0;
+
+// перевірка чи правильно введено
+while (!int.TryParse(strUserNumber, out userNumber))
+{
+    Console.WriteLine("Помилка! Введіть число:");
+    strUserNumber = Console.ReadLine();
+}
+Console.WriteLine($"prime numbers from 1 to {userNumber}:");
+
+for (int i = 2; i <= userNumber; i++)   // цикл на усі числа
+{
+    bool isSimpleNumber = true;
+
+    for (int j = 2; j < i; j++)   // цикл на перевірку дільників
+    {
+        if (i % j == 0)
+        {
+            isSimpleNumber = false;
+        }
+    }
+
+    if (isSimpleNumber)
+    {
+        Console.Write(i + " ");   // виведення простих чисел
+    }
+}
