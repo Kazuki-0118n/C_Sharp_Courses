69177175f64cf8c8a152224ec73e83ee039151dc
Kazuki-0118n <krivogubanastasiya@gmail.com>
2025-12-10 14:30:44 +0200

    Add Homework5.1_2

--- Homework/Homework5.1/Homework5.1_2/Homework5.1_2/Homework5.1_2/Program.cs
+++ Homework/Homework5.1/Homework5.1_2/Homework5.1_2/Homework5.1_2/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;   // щоб було видно українську мову
+
+Console.Write("Введіть ціле число: ");
+
+int rows;
+
+               // перевірка щоб користувач ввів число
+while (!int.TryParse(Console.ReadLine(), out rows))
+{
+    Console.WriteLine("Помилка! Введіть ціле число:");
+}
+
+for (int i = 1; i <= rows; i++)       // цикл на кількість рядків
+{
+    for (int j = 1; j <= i; j++)      //  цикл на зірочки у рядках
+    {
+        Console.Write("✭");
+    }
+    Console.WriteLine();                
+}
