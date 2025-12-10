5acfbcd5083ca9a3baafada8aa1ec592bb380950
Kazuki-0118n <krivogubanastasiya@gmail.com>
2025-12-10 14:05:42 +0200

    Add Homework5.1_6

---
+++ Homework/Homework5.1/Homework5.1_6/Homework5.1_6/Homework5.1_6/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;  // щоб було видно українську
+
+Console.WriteLine("Введіть ціле число: ");
+
+string strUserNumber = Console.ReadLine();       // запит числа від користувача
+int userNumber = 0;
+
+if (int.TryParse(strUserNumber, out userNumber))       
+{
+    Console.WriteLine($"Таблиця множення на {userNumber}");      
+
+    for (int i = 1; i <= 10; i++)                                     // таблиця множення від 1 до 10 на число користувача
+    {
+        Console.WriteLine($"{userNumber} * {i} = {userNumber * i}");
+    }
+}
+                             // якщо було введено не число
+else
+{
+    Console.WriteLine("Не є числом!");
+}
+
+Console.ReadKey();   
