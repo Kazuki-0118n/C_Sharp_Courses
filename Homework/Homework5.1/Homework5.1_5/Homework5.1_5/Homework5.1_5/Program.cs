2bf52545c1544fd6f5012552b8919222cb338624
Kazuki-0118n <krivogubanastasiya@gmail.com>
2025-12-11 16:25:04 +0200

    Update Homework5.1_5

--- Homework/Homework5.1/Homework5.1_5/Homework5.1_5/Homework5.1_5/Program.cs
+++ Homework/Homework5.1/Homework5.1_5/Homework5.1_5/Homework5.1_5/Program.cs
@@ -1,22 +1,55 @@
-﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;  
 
-Console.WriteLine("Введіть кількість робочих годин: ");    
-double hours;
-
-while (!double.TryParse(Console.ReadLine(), out hours))     // перевірка на введення робочих годин
+// -- Ранок
+Console.WriteLine("Введіть кількість ранкових годин:");
+double morningHours;
+while (!double.TryParse(Console.ReadLine(), out morningHours))   // перевірка на введення кількості годин
 {
     Console.WriteLine("Помилка! Введіть число:");
 }
 
-Console.WriteLine("Введіть ставку за годину: ");
-decimal hourlyRate;
-
-while (!decimal.TryParse(Console.ReadLine(), out hourlyRate))    // перевірка на введення годиної ставки
+Console.WriteLine("Введіть ранкову ставку:");
+decimal morningRate;
+while (!decimal.TryParse(Console.ReadLine(), out morningRate))   // перевірка на введення ранкової ставки
 {
     Console.WriteLine("Помилка! Введіть число:");
 }
 
-decimal salary = (decimal)hours * hourlyRate;    // розрахунок оплати за день
+// -- Обід
+Console.WriteLine("Введіть кількість обідніх годин:");
+double dayHours;
+while (!double.TryParse(Console.ReadLine(), out dayHours))    
+{
+    Console.WriteLine("Помилка! Введіть число:");
+}
 
-Console.WriteLine($"Оплата за день: {salary}");
+Console.WriteLine("Введіть обідню ставку:");
+decimal dayRate;
+while (!decimal.TryParse(Console.ReadLine(), out dayRate))   
+{
+    Console.WriteLine("Помилка! Введіть число:");
+}
+
+// -- Вечір
+Console.WriteLine("Введіть кількість вечірніх годин:");
+double eveningHours;
+while (!double.TryParse(Console.ReadLine(), out eveningHours))     
+{
+    Console.WriteLine("Помилка! Введіть число:");
+}
+
+Console.WriteLine("Введіть вечірню ставку:");
+decimal eveningRate;
+while (!decimal.TryParse(Console.ReadLine(), out eveningRate))
+{
+    Console.WriteLine("Помилка! Введіть число:");
+}
+
+// -- Розрахунок
+decimal salary =                                               
+    (decimal)morningHours * morningRate +
+    (decimal)dayHours * dayRate +
+    (decimal)eveningHours * eveningRate;
+
+Console.WriteLine($"\nОплата за день: {salary}");
 Console.ReadKey();
