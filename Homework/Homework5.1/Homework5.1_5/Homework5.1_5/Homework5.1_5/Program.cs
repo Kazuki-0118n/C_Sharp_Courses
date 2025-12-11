f2c026e3f5da19c1a6625c3d1accd34491a3b53f
Kazuki-0118n <krivogubanastasiya@gmail.com>
2025-12-11 10:10:06 +0200

    Add Homework5.1_5

--- Homework/Homework5.1/Homework5.1_5/Homework5.1_5/Homework5.1_5/Program.cs
+++ Homework/Homework5.1/Homework5.1_5/Homework5.1_5/Homework5.1_5/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишіть програму, яка запитує в користувача кількість годин,
+//працюваних за день, та розмір годинної ставки. Після введення значень програма має обчислити та вивести оплату за день.
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Console.WriteLine("Введіть кількість робочих годин: ");
+double hours;
+
+while (!double.TryParse(Console.ReadLine(), out hours))
+{ 
+Console.WriteLine("Помилка! Введіть число:")
+}
+
+Console.WriteLine("Введіть ставку за годину: ");
+decimal hourlyRate;
+
+while (!decimal.TryParse(Console.ReadLine(), out hourlyRate))
+{
+    Console.WriteLine("Помилка! Введіть число!")
+}
+
+decimal salary = hours * hourlyRate
+
+Console.WriteLine($"Заробітна плата: {salary}");
+Console.ReadKey();
