0036a3fa0682d848ece5205bb570f9088d2a2ac2
Kazuki-0118n <krivogubanastasiya@gmail.com>
2025-12-11 12:02:33 +0200

    Update Homework5.1_7

--- Homework/Homework5.1/Homework5.1_7/Homework5.1_7/Homework5.1_7/Program.cs
+++ Homework/Homework5.1/Homework5.1_7/Homework5.1_7/Homework5.1_7/Program.cs
@@ -1,7 +1,59 @@
-﻿Console.WriteLine("Enter a number: ");
-int userNumber = 0;
+﻿var keepRunning = true;
+const string AND_PROG = "exit";
 
-while (!int.TryParse(Console.ReadLine(), out userNumber))
+// -- Введення числа або вихід
+
+while (keepRunning)
 {
-    Console.WriteLine("Error! Enter a number: ");
+    Console.WriteLine("Enter a number or print 'exit': ");
+    string strUserNumber = Console.ReadLine();
+
+    // вихід з циклу
+    if (strUserNumber == AND_PROG)
+    {
+        keepRunning = false;
+    }
+    else
+    {
+        int userNumber;
+
+        while (!int.TryParse(strUserNumber, out userNumber))    //перевірка чи введено число
+        {
+            Console.WriteLine("Error! Enter a number: ");
+        }
+        if (strUserNumber == AND_PROG)
+        {
+            keepRunning = false;
+        }
+
+
+
+        // -- Перевірка чи є простим
+        if (keepRunning)
+        {
+            bool isPrime = true;
+
+            if (userNumber < 2)   // якщо менше 2 не просте
+            {
+                isPrime = false;
+            }
+            else
+            {
+                for (int i = 2; i < userNumber; i++)   // знаходження дільників
+                {
+                    if (userNumber % i == 0)
+                    {
+                        isPrime = false;
+                        break;   // всередині перевірки
+                    }
+                }
+            }
+
+            // виведення результату
+            if (isPrime)
+                Console.WriteLine($"{userNumber} is a prime number.");
+            else
+                Console.WriteLine($"{userNumber} is not a prime number.");
+        }
+    }
 }
