8791cef32a3a63af5222e28b20223dfb5efebcb1
Kazuki-0118n <krivogubanastasiya@gmail.com>
2025-12-12 10:12:09 +0200

    Add Homework5.1_4

--- Homework/Homework5.1/Homework5.1_4/Homework5.1_4/Homework5.1_4/Program.cs
+++ Homework/Homework5.1/Homework5.1_4/Homework5.1_4/Homework5.1_4/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8; 
+
+Console.WriteLine("Введіть кількість чисел Фібоначчі:");
+int n;
+
+// перевірка введення
+while (!int.TryParse(Console.ReadLine(), out n) || n <= 0)
+{
+    Console.WriteLine("Помилка! Введіть додатне ціле число:");
+}
+
+int first = 0;   // перше число
+int second = 1;  // друге число
+
+Console.WriteLine("\nПослідовність Фібоначчі:");
+
+for (int i = 1; i <= n; i++)
+{
+    Console.Write(first + " ");
+
+    int next = first + second; // наступне число
+    first = second;            // зсув значень
+    second = next;
+}
+
+Console.ReadKey();
