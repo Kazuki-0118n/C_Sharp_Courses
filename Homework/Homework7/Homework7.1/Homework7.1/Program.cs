eabb8da8cf1e069fa0773b756a74797ac18f23bd
Kazuki-0118n <krivogubanastasiya@gmail.com>
2025-12-16 11:10:14 +0200

    Apdate Homework7.1

--- Homework/Homework7/Homework7.1/Homework7.1/Program.cs
+++ Homework/Homework7/Homework7.1/Homework7.1/Program.cs
@@ -1,5 +1,17 @@
-﻿
-Random random = new Random();
-
+﻿Random random = new Random();
 int[] randomMasive = new int[10];
 
+// заповнення масиву
+for (int i = 0; i < randomMasive.Length; i++)
+{
+    randomMasive[i] = random.Next(-10, 10);
+}
+
+// виведення елементів з парними індексами
+for (int i = 0; i < randomMasive.Length; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.WriteLine(randomMasive[i]);
+    }
+}
