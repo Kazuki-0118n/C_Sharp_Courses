4844e727fa24ba08d305e811e35400c1467cc3ee
Kazuki-0118n <krivogubanastasiya@gmail.com>
2025-12-16 13:32:22 +0200

    Update Homework7.1

--- Homework/Homework7/Homework7.1/Homework7.1/Program.cs
+++ Homework/Homework7/Homework7.1/Homework7.1/Program.cs
@@ -1,17 +1,39 @@
 ﻿Random random = new Random();
+
+// створення масиву
 int[] randomMasive = new int[10];
 
-// заповнення масиву
+// змінна для суми
+int numberSum = 0;
+
+
+// заповнення масиву випадковими числами
 for (int i = 0; i < randomMasive.Length; i++)
 {
     randomMasive[i] = random.Next(-10, 10);
 }
 
-// виведення елементів з парними індексами
+
+// виведення елементів з парними індексами + підрахунок суми
+Console.WriteLine("Numbers with even index:");
 for (int i = 0; i < randomMasive.Length; i++)
 {
     if (i % 2 == 0)
     {
         Console.WriteLine(randomMasive[i]);
+        numberSum += randomMasive[i];
     }
+}
+
+
+// сума 
+Console.WriteLine("Is the sum of the numbers a non-negative number?");
+
+if (numberSum >= 0)
+{
+    Console.WriteLine("Yes. It's a non-negative number.");
+}
+else
+{
+    Console.WriteLine("No. It's a negative number.");
 }
