cbde035e4245d80b886003c14b059ed1c39e2dc4
Kazuki-0118n <krivogubanastasiya@gmail.com>
2025-12-16 16:28:31 +0200

    Add Homework_task3

---
+++ Homework/Homework7/Homework_task3/Homework_task3/Program.cs
@@ -0,0 +1,21 @@
+﻿int[,] table = new int[9, 9];
+
+for (int i = 0; i < 9; i++)    
+{
+    for (int j = 0; j < 9; j++)   
+    {
+        table[i, j] = (i + 1) * (j + 1);
+    }
+}
+
+// виведення результату множення
+for (int i = 0; i < 9; i++)
+{
+    for (int j = 0; j < 9; j++)
+    {
+        Console.Write(table[i, j] + "\t");
+    }
+    Console.WriteLine();
+}
+
+
