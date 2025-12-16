0ab36b88d8303c7420dab27e473f7a0de32ec022
Kazuki-0118n <krivogubanastasiya@gmail.com>
2025-12-16 19:22:41 +0200

    Add Task4

---
+++ Homework/Homework7/Task4/Task4/Program.cs
@@ -0,0 +1,14 @@
+﻿Random random = new Random();
+
+int[,] yakiisMasiv = new int[5, 5];
+
+for (int i = 0; i < 5; i++)           // строки
+{
+    for (int j = 0; j < 5; j++)       // стовпці
+    {
+        yakiisMasiv[i, j] = random.Next(-100, 100);
+        Console.Write(yakiisMasiv[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+
