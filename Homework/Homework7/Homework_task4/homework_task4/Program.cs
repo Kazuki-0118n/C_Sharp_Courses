0f628e5ba778e99698741082c36869125c6d7519
Kazuki-0118n <krivogubanastasiya@gmail.com>
2025-12-18 12:20:41 +0200

    And Homework7_task3

--- Homework/Homework7/Homework_task4/homework_task4/Program.cs
+++ Homework/Homework7/Homework_task4/homework_task4/Program.cs
@@ -1,10 +1,57 @@
 ﻿// створення масиву
-int[,] yakiisMasiv = new int[5, 5];
+int[,] yakiisMasiv =
+{
+    { 99, 27, 3, 8, 10 },
+    { 2, 1, 4, 67, 739 },
+    { 9, 122, 11, 0, 7 },
+    { 4, 455, 8, 12, 1 },
+    { 6, 666, 81, 43, 5 }
+};
 
-int[,] yakiisMasiv = { { -7, 27 }, { 2, 1 }, { 9, 122 }, { 4, 455 }, { 6, 666 } },
-                     { { 5, 83 }, { -1, 67 }, { 8, 145 }, { -6, -666 }, { 77, 99 } }, 
-                     { { 3, 4}, { 12, 56 }, { -9, -41 }, { 51, 86 }, { -82, 672} },
-                     { { 24, 455 }, { 49, - 89 }, { 31, 833 }, { 29, 231 }, { 622, 1802 } },
-                     { { 34, 2221 }, { 678, 74 }, {597, 47 }, { 26, 70 }, { 367, 89} };
+int max = 0;
+int min = 100000;
 
+foreach(int num in yakiisMasiv)
+{
+    if (num > max)           // порівнює для знаходження найбільшого числа
+    {
+        max = num;
+    }
+    if (num < min)           // порівнює для знаходження найменшого числа
+    {
+        min = num;
+    }
+}
 
+int targetMax = max;
+int targetMin = min;
+
+// найбільше число виведення
+Console.WriteLine($"The largest number: {max}");
+
+// -- знаходження координат найбільного числа
+for (int i = 0; i < 5; i++)  // рядки
+{
+    for (int j = 0; j < 5; j++)  // стовпці 
+    {
+        if (yakiisMasiv[i, j] == targetMax)
+        {
+            Console.WriteLine($"Coordinates: [{i}, {j}]"); 
+        }
+    }
+}
+
+// найменше число виведення
+Console.WriteLine($"The lowest number: {min}");
+
+// -- знаходження координат найменшого числа
+for (int i = 0; i < 5; i++)  // рядки
+{
+    for (int j = 0; j < 5; j++)  // стовпці 
+    {
+        if (yakiisMasiv[i, j] == targetMin)
+        {
+            Console.WriteLine($"Coordinates: [{i}, {j}]");
+        }
+    }
+}
