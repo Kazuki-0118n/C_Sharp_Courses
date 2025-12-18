3e05e91f6816cc75c2efe607e0833c3c51313772
Kazuki-0118n <krivogubanastasiya@gmail.com>
2025-12-18 16:30:57 +0200

    update homework7.2

---
+++ Homework/Homework7/Homework7.2(unfinished)/Homework7.2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+int[,] game =
+{
+    { 1, 2, 3 },
+    { 4, 5, 6 },
+    { 7, 8, 9 }
+};
+
+
+//var firstPleyer = x;
+//var secondPleyer = o;
+
+// -- Створення ігрового поля
+for (int i = 0; i < 3; i++)
+{ 
+    for (int j = 0; j < 3; j++)
+    {
+        Console.Write(game[i, j] + " | ");
+    }
+    Console.WriteLine();
+    Console.WriteLine("----------.");
+}
+
+
