a63980ebeadf5cd6b654a6bac860f99077d8f3d5
Kazuki-0118n <krivogubanastasiya@gmail.com>
2025-12-19 11:20:30 +0200

    update Homework7.2

--- Homework/Homework7/Homework7.2(unfinished)/Homework7.2/Program.cs
+++ Homework/Homework7/Homework7.2(unfinished)/Homework7.2/Program.cs
@@ -1,19 +1,22 @@
 ﻿using System;
 Console.OutputEncoding = System.Text.Encoding.UTF8;
-bool someoneWin = true;
 
-int[,] game =
+bool someoneWin = true;
+char pleyerX = 'x';
+char pleyerY = 'y';
+
+char[,] game =
 {
-    { 1, 2, 3 },
-    { 4, 5, 6 },
-    { 7, 8, 9 }
+    { '1', '2', '3' },
+    { '4', '5', '6' },
+    { '7', '8', '9' }
 };
 
 
 //string firstPleyer = x;
 //string secondPleyer = o;
-int pleyerOne_Choose;
-int pleyerTwo_Choose;
+char pleyerOne_Choose;
+char pleyerTwo_Choose;
 
 
 // -- Створення ігрового поля
@@ -21,12 +24,43 @@ for (int i = 0; i < 3; i++)
 { 
     for (int j = 0; j < 3; j++)
     {
-        Console.Write(game[i, j] + " | ");
+        char field = game[i, j];
+        Console.Write($"{field} | ");
     }
     Console.WriteLine();
     Console.WriteLine("----------.");
+}
 
-};
+// перший хід
+Console.WriteLine("Хід першого гравця.Введіть номер клітинки для ходу: ");
+char input;
+while (!char.TryParse(Console.ReadLine(), out input))
+{
+    Console.WriteLine("Введіть одне число: ");
+}
 
-Console.WriteLine("Хід першого гравця. Оберіть число для ходу:");
-pleyerOne_Choose = int.TryParse(Console.ReadLine());
+char playerOne_Choose = input;
+
+// виведення x у поле
+for (int i = 0; i < 3; i++)
+{
+    for (int j = 0; j < 3; j++)
+    {
+        if (game[i, j] == playerOne_Choose)
+        {
+            game[i, j] = pleyerX;
+        }
+    }
+}
+
+// оновлення поля
+for (int i = 0; i < 3; i++)
+{
+    for (int j = 0; j < 3; j++)
+    {
+        char field = game[i, j];
+        Console.Write($"{field} | ");
+    }
+    Console.WriteLine();
+    Console.WriteLine("----------.");
+}
