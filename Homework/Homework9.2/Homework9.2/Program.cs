05a6b758422ff3e777b68885f07fe57ae23b31d6
Kazuki-0118n <krivogubanastasiya@gmail.com>
2025-12-26 14:18:36 +0200

    add date to report

--- Homework/Homework9.2/Homework9.2/Program.cs
+++ Homework/Homework9.2/Homework9.2/Program.cs
@@ -1,4 +1,18 @@
 ﻿using System;
+using System.Text;
 Console.OutputEncoding = System.Text.Encoding.UTF8;
 
-Console.WriteLine("Звіт про кількість з'їдених бубликів");
+StringBuilder report = new StringBuilder("ЗВІТ\nРЕЗУЛЬТАТ ЧИТАННЯ МАНГИ.");
+
+// дадання дати
+string dateReport = "\nДата прочитання останьої глави:";
+report.Append(dateReport);
+
+Console.WriteLine("Дата прочитання останьої глави:");
+string date = Console.ReadLine();
+string paddedDate = date.PadLeft(15);
+report.Append(paddedDate);
+
+
+
+Console.WriteLine(report.ToString());
