d1521c11694627c3516c57af4b343e0b680df67c
Kazuki-0118n <krivogubanastasiya@gmail.com>
2025-12-26 12:34:32 +0200

    Add Homework9.1

---
+++ Homework/Homework9.1/Homework9.1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Console.WriteLine("Введіть ваше ім'я:");
+string userFirstName  = Console.ReadLine();
+
+Console.WriteLine("Введіть ваше прізвище:");
+string userLastName = Console.ReadLine();
+
+if (userFirstName[0] == userLastName[0])
+{
+    Console.WriteLine("Прізвище починається на ту ж літеру, що і ім’я");
+}
+else
+{
+    Console.WriteLine("Прізвище не починається на ту ж літеру, що і ім’я");
+}    
+
+
