4d5780495df7ea0af71e8a3bf90e1d1e92f6416d
Kazuki-0118n <krivogubanastasiya@gmail.com>
2025-12-26 15:01:45 +0200

    add events in homework9.2

--- Homework/Homework9.2/Homework9.2/Program.cs
+++ Homework/Homework9.2/Homework9.2/Program.cs
@@ -5,7 +5,7 @@ Console.OutputEncoding = System.Text.Encoding.UTF8;
 StringBuilder report = new StringBuilder("ЗВІТ\nРЕЗУЛЬТАТ ЧИТАННЯ МАНГИ.");
 
 // дадання дати
-string dateReport = "\nДата прочитання останьої глави:";
+string dateReport = "\nДата:";
 report.Append(dateReport);
 
 Console.WriteLine("Дата прочитання останьої глави:");
@@ -13,6 +13,28 @@ string date = Console.ReadLine();
 string paddedDate = date.PadLeft(15);
 report.Append(paddedDate);
 
+
+// додавання подій
+bool isAdds = true;
+const string EVENTS_OVER = "exit";
+int eventsNum = 1;
+
+while (isAdds)
+{
+    Console.WriteLine("Введіть подію, якщо події закінчились введіть 'exit': ");
+    var userEvents = Console.ReadLine();
+    if (userEvents == EVENTS_OVER)
+    {
+        isAdds = false;       // вихід з введення подій
+
+    }
+    else
+    {
+        string events = $"\nПодія {eventsNum}:  {userEvents}";
+        report.AppendLine(events);
+        eventsNum ++;
+    }
+}
 
 
 Console.WriteLine(report.ToString());
