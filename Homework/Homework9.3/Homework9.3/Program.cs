a8d7dd658ada920fe680d449b2ebd9fcd1f53ee7
Kazuki-0118n <krivogubanastasiya@gmail.com>
2025-12-26 13:32:26 +0200

    finished homework9.3

---
+++ Homework/Homework9.3/Homework9.3/Program.cs
@@ -0,0 +1,13 @@
+﻿string someProducts = "kartoshka, sosiska, bagato hliba, slabo-solenii ogurchik, yakiis tortik/pirog";
+Console.WriteLine(someProducts);
+
+// прибирає знаки між словами
+string result = someProducts
+    .Replace(" ", "")
+    .Replace(",", "")
+    .Replace("-", "")
+    .Replace(", ", "")
+    .Replace(".", "")
+    .Replace("/", "");
+
+Console.WriteLine(result);
