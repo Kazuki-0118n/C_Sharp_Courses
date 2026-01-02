b7aa6cc7cd44959f6f0fa8195a38f1be0a10e3d3
Kazuki-0118n <krivogubanastasiya@gmail.com>
2026-01-02 13:57:15 +0200

    Initial game setup

--- Homework/Homework10.1/Homework10.1/Program.cs
+++ Homework/Homework10.1/Homework10.1/Program.cs
@@ -1,1 +1,7 @@
-﻿
+﻿int numberOfAttempts = 5;
+Console.WriteLine($"Welcome! Try to guess the hidden word!\nNumber of letters in the word: 5\nNumber of allowed incorrect attempts: {numberOfAttempts}");
+
+string encryptedWord = "magic";
+
+Console.Write("Enter a letter: ");
+Console.ReadLine();
