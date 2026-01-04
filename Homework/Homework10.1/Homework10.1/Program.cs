c778467c1bd10fef88a7e0f16ade1e81e6fbdbbf
Kazuki-0118n <krivogubanastasiya@gmail.com>
2026-01-04 14:29:52 +0200

    Select encrypted word based on game mode

--- Homework/Homework10.1/Homework10.1/Program.cs
+++ Homework/Homework10.1/Homework10.1/Program.cs
@@ -25,62 +25,74 @@ while(!isValid)
     }
 }
 
-string encryptedWord = "magic";
-char[] guessedWord = { ' ', ' ', ' ', ' ', ' ' };
+string encryptedWord;
 
-
-while ( numberOfAttempts > 0 )
+if (userSelectMode == '1')
 {
-    Console.Write("Enter a letter: ");
-    char userLetter = ' ';
-    while (!char.TryParse(Console.ReadLine(), out userLetter))
-    {
-        Console.WriteLine("Enter one letter!");
-    }
-
-    int index = encryptedWord.IndexOf(userLetter);
-
-    if (index != -1)
-    {
-        Console.WriteLine($"This letter is in the word! Letter position: {index + 1}");
-
-        // додає літеру у масив
-        bool found = false;
-
-        for (int i = 0; i < encryptedWord.Length; i++)
-        {
-            if (encryptedWord[i] == userLetter)
-            {
-                guessedWord[i] = userLetter;
-                found = true;
-            }
-        }
-        // перевірка вийграшу
-        int openedLetters = 0;
-
-        for (int i = 0; i < guessedWord.Length; i++)
-        {
-            if (guessedWord[i] != ' ')
-            {
-                openedLetters++;
-            }
-        }
-
-        if (openedLetters == guessedWord.Length)
-        {
-            Console.WriteLine();
-            Console.WriteLine($"Congratulations, you guessed the word! Hidden word: {encryptedWord}");
-            Console.WriteLine("Thank you for playing.");
-            break;
-        }
-    }
-    else
-    {
-        numberOfAttempts--;
-        Console.WriteLine($"There is no such letter! Remaining attempts: {numberOfAttempts}");
-    }
-
+    encryptedWord = "star";
 }
+else if (userSelectMode == '2')
+{
+    encryptedWord = "water";
+}
+else if (userSelectMode == '3')
+{
+    encryptedWord = "planet";
+}
+
+
+    while (numberOfAttempts > 0)
+    {
+        Console.Write("Enter a letter: ");
+        char userLetter = ' ';
+        while (!char.TryParse(Console.ReadLine(), out userLetter))
+        {
+            Console.WriteLine("Enter one letter!");
+        }
+
+        int index = encryptedWord.IndexOf(userLetter);
+
+        if (index != -1)
+        {
+            Console.WriteLine($"This letter is in the word! Letter position: {index + 1}");
+
+            // додає літеру у масив
+            bool found = false;
+
+            for (int i = 0; i < encryptedWord.Length; i++)
+            {
+                if (encryptedWord[i] == userLetter)
+                {
+                    guessedWord[i] = userLetter;
+                    found = true;
+                }
+            }
+            // перевірка вийграшу
+            int openedLetters = 0;
+
+            for (int i = 0; i < guessedWord.Length; i++)
+            {
+                if (guessedWord[i] != ' ')
+                {
+                    openedLetters++;
+                }
+            }
+
+            if (openedLetters == guessedWord.Length)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Congratulations, you guessed the word! Hidden word: {encryptedWord}");
+                Console.WriteLine("Thank you for playing.");
+                break;
+            }
+        }
+        else
+        {
+            numberOfAttempts--;
+            Console.WriteLine($"There is no such letter! Remaining attempts: {numberOfAttempts}");
+        }
+
+    }
 
 if (numberOfAttempts == 0)
 {
@@ -88,3 +100,4 @@ if (numberOfAttempts == 0)
     Console.WriteLine($"You lost! The hidden word was: {encryptedWord}");
     Console.WriteLine("Thank you for playing.");
 }
+ 
