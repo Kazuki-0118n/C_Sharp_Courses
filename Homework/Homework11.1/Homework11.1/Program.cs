3aad8f05dda1b35ede54325e81e70ed7ae5d1ef9
Kazuki-0118n <krivogubanastasiya@gmail.com>
2026-01-06 17:03:57 +0200

    Add recursive Fibonacci method

---
+++ Homework/Homework11.1/Homework11.1/Program.cs
@@ -0,0 +1,12 @@
+﻿int Fibonacсi(int num)
+{
+    if (num == 1)
+        return 0;
+
+    if (num == 2)
+        return 1;
+
+    return Fibonacсi(num - 1) + Fibonacсi(num - 2);
+}
+
+Console.WriteLine(Fibonacсi(11));
