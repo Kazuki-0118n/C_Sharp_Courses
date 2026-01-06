2cd2c90a7c0197cf4e96152f01127066702e8be2
Kazuki-0118n <krivogubanastasiya@gmail.com>
2026-01-06 17:32:36 +0200

    Add user input for Fibonacci

--- Homework/Homework11.1/Homework11.1/Program.cs
+++ Homework/Homework11.1/Homework11.1/Program.cs
@@ -1,4 +1,14 @@
-﻿int Fibonacсi(int num)
+﻿int userNum;
+bool parsed;
+
+do
+{
+    Console.Write("Enter a positive number: ");
+    parsed = int.TryParse(Console.ReadLine(), out userNum);
+
+} while (!parsed || userNum <= 0);
+
+int Fibonacсi(int num)
 {
     if (num == 1)
         return 0;
@@ -9,4 +19,4 @@
     return Fibonacсi(num - 1) + Fibonacсi(num - 2);
 }
 
-Console.WriteLine(Fibonacсi(11));
+Console.WriteLine(Fibonacсi(userNum));
