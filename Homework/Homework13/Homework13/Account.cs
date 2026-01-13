cb3f353cafa732d2a5bb26b6457a0d7f549159a2
Kazuki-0118n <krivogubanastasiya@gmail.com>
2026-01-13 16:12:49 +0200

    Implement basic bank account logic: Withdrawal and Deposit

---
+++ Homework/Homework13/Homework13/Account.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework13
+{
+    internal class Account
+    {
+        private string name;
+        private decimal balance = 1000;
+
+
+        public bool Withdrawal(decimal amount)
+        {
+            if (amount <= balance)
+            {
+                balance -= amount;
+                Console.WriteLine($"Success! Remaining balance: {balance}");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Error! Insufficient funds!");
+                return false;
+            }
+        }
+
+
+        public bool Deposit(decimal amount)
+        {
+                balance += amount;
+                Console.WriteLine($"Success! Remaining balance: {balance}");
+                return true;
+        }
+    }
+}
---
+++ Homework/Homework13/Homework13/Program.cs
@@ -0,0 +1,14 @@
+﻿using Homework13;
+
+Account myAccount = new Account();
+
+Console.WriteLine("Enter amount:");
+
+if (decimal.TryParse(Console.ReadLine(), out decimal userInput))
+{
+    myAccount.Withdrawal(userInput);
+}
+else
+{
+    Console.WriteLine("Invalid input!");
+}
