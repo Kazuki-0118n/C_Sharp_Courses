f187a0dab2c724195b2b8a3006b09913a076b703
Kazuki-0118n <krivogubanastasiya@gmail.com>
2026-01-13 16:33:59 +0200

    Add switch case deposit and withdrawal methods

--- Homework/Homework13/Homework13/Program.cs
+++ Homework/Homework13/Homework13/Program.cs
@@ -2,13 +2,45 @@
 
 Account myAccount = new Account();
 
-Console.WriteLine("Enter amount:");
+bool isRunning = true;
 
-if (decimal.TryParse(Console.ReadLine(), out decimal userInput))
+while (isRunning)
 {
-    myAccount.Withdrawal(userInput);
-}
-else
-{
-    Console.WriteLine("Invalid input!");
+    Console.WriteLine("--- ACCOUNT MENU ---");
+    Console.WriteLine("1 - View Balance");
+    Console.WriteLine("2 - Withdraw");
+    Console.WriteLine("3 - Deposit");
+    Console.WriteLine("4 - Change Name");
+    Console.WriteLine("5 - Exit");
+    Console.Write("\nSelect an option: ");
+
+    string input = Console.ReadLine();
+
+    switch (input)
+    {
+        case "2":
+            Console.WriteLine("Enter amount:");
+            if (decimal.TryParse(Console.ReadLine(), out decimal userInputWithdr))
+            {
+                myAccount.Withdrawal(userInputWithdr);
+            }
+            else
+            {
+                Console.WriteLine("Invalid input!");
+            }
+            break;
+
+        case "3":
+            Console.WriteLine("Enter amount:");
+            if (decimal.TryParse(Console.ReadLine(), out decimal userInputDepo))
+            {
+                myAccount.Deposit(userInputDepo);
+            }
+            else
+            {
+                Console.WriteLine("Invalid input!");
+            }
+            break;
+            break;
+    }
 }
