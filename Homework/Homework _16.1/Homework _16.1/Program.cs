33cb15e0a403da9b7cf06d8ae026e359efcd70f3
Kazuki-0118n <krivogubanastasiya@gmail.com>
2026-01-25 15:03:14 +0200

    Add user input verification -- Add divede calculator with error checking

---
+++ Homework/Homework _16.1/Homework _16.1/Program.cs
@@ -0,0 +1,42 @@
+﻿double firstNum;
+double secondNum;
+double result = 0;
+
+Console.Write("Enter first number: ");
+while (!double.TryParse(Console.ReadLine(), out firstNum))
+{
+    Console.Write("Error! Enter a number: ");
+}
+
+Console.Write("Enter second number: ");
+while (!double.TryParse(Console.ReadLine(), out secondNum))
+{
+    Console.Write("Error! Enter a number: ");
+}
+
+bool success = false;
+
+try
+{
+    result = firstNum / secondNum;
+    success = true;
+}
+catch (DivideByZeroException)
+{
+    Console.WriteLine("You can't divide by zero!");
+}
+catch (Exception)
+{
+    Console.WriteLine("Error in calculation.");
+}
+finally
+{
+    if (success)
+    {
+        Console.WriteLine($"The result is: {result}");
+    }
+    else
+    {
+        Console.WriteLine("Calculation failed, so no result to show.");
+    }
+}
